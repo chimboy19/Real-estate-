899a99ff53aed56ebf9fefbf3c0a3e68ed62933c
Your Name <chimanancy16@gmail.com>
2024-10-07 06:38:46 -0700

    fixed

--- remix.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  serverBuildTarget: "netlify", // This is specific for Netlify
-  publicPath: "/build/",
-  assetsBuildDirectory: "public/build",
-  serverBuildDirectory: "netlify/functions/server/build",
-  devServerPort: 8002,
-};
